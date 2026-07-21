b8e0bb704d59469cb54269cd5beb2250af61e0cc
Serj <serj@MacBook-Air-Serj.local>
2026-07-21 23:24:34 +0300

    edit MainPage

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,7 +1,6 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue';
 import VideoContent from '@/components/VideoContent.vue';
-import BigCard from '@/components/BigCard.vue';
 import AppFooter from '@/components/AppFooter.vue';
 import AppAbout from '@/components/AppAbout.vue';
 import AppServices from '@/components/AppServices.vue';
